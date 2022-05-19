c17b4ef6752f6a9101ca7b5b6eb6a2361d207cfb
Nik090595 <nikitanakade1@gmail.com>
2022-05-19 22:38:27 +0530

    New Code added TestNG Class

---
+++ src/com/POM/New_POM.java
@@ -0,0 +1,11 @@
+package com.POM;
+
+public class New_POM {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is new TestNG class");
+		System.out.println("for GitHub");
+	}
+	
+}
